bc3a8c23e9420a20ccfe6bb97c19e2a7db5dca9b
BaReinhard <brett@reinhards.us>
2018-09-30 14:06:26 -0700

    Added index handler

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
 
 func main() {
-	fmt.Print("Success")
+	http.HandleFunc("/", indexHandler)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Success"))
 }
