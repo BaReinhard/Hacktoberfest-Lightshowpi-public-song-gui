0ac103835b0006e11223e0a189a8b0c06ca1c0e7
BaReinhard <brett@reinhards.us>
2018-09-30 18:03:31 -0700

    Updated basic frontend code to display current song and playlist songs, json format is still being considered

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,58 +2,64 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import axios from "axios";
 import "./App.css";
-
+function isEmpty(obj) {
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) return false;
+  }
+  return true;
+}
 class App extends Component {
   constructor() {
+    super();
     this.state = {
-      psgState: {}
+      psgState: {
+        running: false
+      }
     };
   }
   async componentDidMount() {
     let newState = {};
-    if (process.env.PSG_ENV === "PROD") {
+    if (process.env.NODE_ENV === "PROD") {
       newState = await axios.get("/api/getState").then(res => {
-        res;
         if (res.data) {
           return res.data;
         }
       });
     } else {
       newState = await new Promise((resolve, reject) => {
-        return {
+        resolve({
           songs: [
             { name: "First Song", artist: "First Artist" },
             { name: "Second Song", artist: "Second Artist" },
             { name: "Third Song", artist: "Third Artist" }
           ],
-          status: "playing",
+          running: true,
           currentSongIndex: 0,
           currentSong: { name: "First Song", artist: "First Artist" }
-        };
+        });
       });
     }
     this.setState({ psgState: newState });
   }
   render() {
-    if (this.state.psgState) {
+    let AppHead = () => (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to Lightshow Pi</h1>
+      </header>
+    );
+
+    if (!this.state.psgState.running) {
       return (
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+          <AppHead />
+          <p className="App-intro">The lightshow is currently unavailable</p>
         </div>
       );
     } else {
       return (
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
+          <AppHead />
           <div>
             <h1>Current Song</h1>
             <div>
